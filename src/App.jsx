6c7a94da1fed2ad49398aae7a20d046d19e9785a
Naaati <118106683+Naaati@users.noreply.github.com>
2023-06-05 22:24:08 +0200

    Dodanie plików projektu

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <title>Vite</title>
+      <div>
+        <h1>Natalia Oleska </h1>
+        <h2>Nr albumu 113850</h2>
+      </div>   
+    </>
+  )
+}
+
+export default App
